e1309b8d75640cae0e957c8136626330b1c712c7
Miguel Belbute <118909502+zorrocrisis@users.noreply.github.com>
2024-08-01 22:42:14 +0100

    Add files via upload

---
+++ assets/js/code.js
@@ -0,0 +1,43 @@
+var numSliders = 8;
+var slideList = [];
+
+for(var l = 0; l < numSliders; l++)
+{
+    slideList[l] = l * 3; /* assuming 3 pictures per slider*/
+}
+
+
+for(var l = 0; l < slideList.length; l++)
+{
+    SlideShow(l);
+}
+
+
+function plusSlides(n, setIndex) {
+    slideList[setIndex] = slideList[setIndex] + n;
+    SlideShow(setIndex);
+}
+
+
+//  images controls
+function currentSlide(n) {
+  SlideShow(n, 0);
+}
+
+function SlideShow(index) {
+    var i;
+    var slides = document.getElementsByClassName("Containers");
+
+    var lowerBound = index * 3;
+    var upperBound = lowerBound + 2;
+
+    if (slideList[index] > upperBound) {slideList[index] = lowerBound}
+    if (slideList[index] < lowerBound) {slideList[index] = upperBound}
+    
+    for (i = lowerBound; i < upperBound + 1; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideList[index]].style.display = "block";
+
+} 
